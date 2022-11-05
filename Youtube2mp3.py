e9413d206671c70b00cfa36c895d966b65aa798c
BigTSmallHardwareStore <tobias-kolb@t-online.de>
2022-11-05 09:40:34 +0100

    Add existing project files to Git

---
+++ Youtube2mp3.py
@@ -0,0 +1,14 @@
+from pytube import YouTube, Playlist
+
+
+
+
+
+bitrate = '128kbps'
+
+
+
+if __name__ == '__main__':
+    # choose if one video or a playlist
+    decision = input("If you want to download a single video, enter 1. \nIf you want to download a whole playlist, enter 2. \n")
+    pass
