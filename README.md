cb0e7fe399b38b550ed7111b49fea6c823a24678
BigTSmallHardwareStore <tobias-kolb@t-online.de>
2022-11-06 08:50:40 +0100

    Version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Youtube_Downloader
-A small program to download videos from Youtube as mp3. 
+# YouTube Downloader
+A small program to download videos from YouTube as mp3.
+
+> ##### For legal purposes:
+> Check your local laws and the terms of YouTube before using this tool!
--- Youtube2mp3.py
+++ Youtube2mp3.py
@@ -1,14 +1,47 @@
 from pytube import YouTube, Playlist
+import moviepy.editor
+import os
 
 
+def download_playlist(url: str):
+    playlist = Playlist(url)
+    for video in playlist.videos:
+        filename = video.streams.get_audio_only().download()
+
+        convert_mp4_to_mp3(filename=filename)
 
 
+def download_single_video(url: str):
+    music = YouTube(url)
+    filename = music.streams.get_audio_only().download()
 
-bitrate = '128kbps'
+    convert_mp4_to_mp3(filename=filename)
 
+
+def convert_mp4_to_mp3(filename: str):
+    clip = moviepy.editor.AudioFileClip(filename)
+
+    # rename file from mp4 to mp3
+    name, _ = os.path.splitext(filename)
+    new_filename = name + '.mp3'
+
+    clip.write_audiofile(new_filename)
+    clip.close()
+
+    # delete unneeded mp4 file
+    os.remove(filename)
 
 
 if __name__ == '__main__':
     # choose if one video or a playlist
-    decision = input("If you want to download a single video, enter 1. \nIf you want to download a whole playlist, enter 2. \n")
-    pass
+    decision = int(input("If you want to download a single video, enter 0 \nIf you want to download a whole playlist, enter 1 \n"))
+    url = input("Enter URL here: ")
+
+    # match input to correct download function
+    match decision:
+        case 0:  # single video
+            download_single_video(url=url)
+        case 1:  # playlist
+            download_playlist(url=url)
+        case _:
+            print('Invalid input')
