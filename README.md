2f935c59e0273afa907c5deda4c69e944bfd63c5
BigTSmallHardwareStore <tobias-kolb@t-online.de>
2022-11-21 17:37:30 +0100

    add function to download multiple playlists

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # YouTube Downloader
-A small program to download videos from YouTube as mp3.
+A small program to download videos or playlists from YouTube as mp3.
 
 > ##### For legal purposes:
 > Check your local laws and the terms of YouTube before using this tool!
--- Youtube2mp3.py
+++ Youtube2mp3.py
@@ -1,12 +1,45 @@
+import shutil
 from pytube import YouTube, Playlist
 import moviepy.editor
 import os
+import pandas as pd
+import sys
+import time
+
+
+def download_multiple_playlist():
+    # try to load list of playlists
+    try:
+        df = pd.read_csv('playlists.csv', header=None)
+    except FileNotFoundError:
+        print('File not found')
+        time.sleep(5)
+        sys.exit()
+
+    # iterate over rows in list
+    for index, row in df.iterrows():
+        playlist = Playlist(row[0])
+
+        # check if playlist title exists as folder and create it if not
+        if not os.path.exists(playlist.title):
+            os.mkdir(playlist.title)
+
+        for video in playlist.videos:
+            try:
+                filename = video.streams.get_audio_only().download()
+
+                mp3_filename = convert_mp4_to_mp3(filename=filename)
+
+                # move file to folder
+                shutil.move(mp3_filename, os.path.join(playlist.title, os.path.basename(mp3_filename)))
+            except:
+                continue
 
 
 def download_playlist(url: str):
     playlist = Playlist(url)
     for video in playlist.videos:
-        filename = video.streams.get_audio_only().download()
+        filename = video.streams.get_audio_only()
 
         convert_mp4_to_mp3(filename=filename)
 
@@ -31,11 +64,16 @@ def convert_mp4_to_mp3(filename: str):
     # delete unneeded mp4 file
     os.remove(filename)
 
+    return new_filename
+
 
 if __name__ == '__main__':
     # choose if one video or a playlist
-    decision = int(input("If you want to download a single video, enter 0 \nIf you want to download a whole playlist, enter 1 \n"))
-    url = input("Enter URL here: ")
+    decision = int(input("If you want to download a single video, enter 0 \n"
+                         "If you want to download a whole playlist, enter 1 \n"
+                         "If you want to download multiple playlists, enter 9 \n"))
+    if decision != 9:
+        url = input("Enter URL here: ")
 
     # match input to correct download function
     match decision:
@@ -43,5 +81,7 @@ if __name__ == '__main__':
             download_single_video(url=url)
         case 1:  # playlist
             download_playlist(url=url)
+        case 9:
+            download_multiple_playlist()
         case _:
             print('Invalid input')
